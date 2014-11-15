45108bceb7c22f71ab047a9924d6b8ff255d9367
Noah Tye <noah@tyes.us>
2014-11-15 01:02:59 -0800

    posting commentary works, mostly (editing is broken)

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE commentary (
+  book TEXT,
+  chapter INTEGER,
+  verse INTEGER,
+  text TEXT
+);
+
+INSERT INTO commentary VALUES('genesis', 1, 4, 'separation');
--- server.py
+++ server.py
@@ -1,7 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, g
 import requests
+import sqlite3
+import json
+
+DATABASE = '/Users/noah/soren/database.db'
 
 app = Flask(__name__, static_folder='/Users/noah/soren')
+
+def get_db():
+  db = getattr(g, '_database', None)
+  if db is None:
+    db = g._database = sqlite3.connect(DATABASE)
+  db.row_factory = sqlite3.Row # is this the right place for this line?
+  return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+  db = getattr(g, '_database', None)
+  if db is not None:
+    db.close()
+
+def query_db(query, args=(), one=False):
+  cur = get_db().execute(query, args)
+  rv = cur.fetchall()
+  cur.close()
+  return (rv[0] if rv else None) if one else rv
 
 @app.route("/")
 def root():
@@ -23,7 +46,23 @@ def passage():
       "include-simple-entities": "true",
       "output-format": "crossway-xml-1.0"
     })
+  # remove the DOCTYPE which for some reason breaks jQuery parsing
   return r.text.replace('<!DOCTYPE crossway-bible SYSTEM "http://www.gnpcb.org/esv/share/schemas/crossway.base.entities.dtd">', '')
 
+def label(t, *labels):
+  '''Given a tuple and a list of objects, produce a '''
+
+@app.route("/commentary", methods=['GET', 'POST'])
+def commentary():
+  if request.method == 'POST':
+    get_db().execute('INSERT INTO commentary VALUES(?, ?, ?, ?)',
+                     (request.form["book"], request.form["chapter"], request.form["verse"], request.form["text"]))
+    get_db().commit()
+    return 'ok'
+  elif request.method == 'GET':
+    print request.args.get('book')
+    results = query_db('select * from commentary')
+    return json.dumps([dict(zip(['book', 'chapter', 'verse', 'comment'], r)) for r in results])
+
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
